4f42e43e5683a22203b2f785bc3f9d03295bcdb9
Chiaya T <709210chiaya@gmail.com>
2023-07-13 15:17:19 -0600

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+  int year = 2003;
+  String month = "December";
+
+  System.out.println(year);
+  System.out.println(month);
+  System.out.println(false);
+
+  }
+}
