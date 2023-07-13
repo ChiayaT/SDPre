559aa3c5ebe1b108394ecda400491b58b311b93b
Chiaya T <709210chiaya@gmail.com>
2023-07-13 15:08:14 -0600

    finish Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Chiaya Thach";
+    int age = 19;
+    String state = "Colorado";
+
+    System.out.println(name +" Age: " + age + " State: " + state);
+  }
+}
