773186b702941b773e1ff0b870ef8d0b775cba58
Chiaya T <709210chiaya@gmail.com>
2023-07-13 22:29:15 -0600

    finish Objective6lab

---
+++ Labs/Objective6Lab.java
@@ -0,0 +1,21 @@
+public class Objective6Lab {
+  public static void main(String[] args) {
+    int month = 6;
+
+    switch (month) {
+      case 1:
+        System.out.println("January");
+        break;
+
+        /* Do this 1-12 for all the months */
+
+      case 6:
+        System.out.println("June");
+        break;
+
+      default:
+        System.out.println("That is not a valid month");
+        break;
+    }
+  }
+}
